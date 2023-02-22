74374799c4e3c34051adc8b35e25b1b2c1810cfe
DanielleUju <danielleObilor1@gmail.com>
2023-02-22 04:43:54 -0800

    Nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,18 @@
 
 void print_alphabet_x10(void)
 {
-	char c = 'a'
+	char c = 'a';
+	int j = 1;
 
 	while (c <= 'z')
 	{
+		while (j <= 10)
+		{
+			_putchar(c);
+			j++;
+		}
 		_putchar(c);
 		c++;
 	}
-		_putchar('\n');
-
+	_putchar('\n')
 }
