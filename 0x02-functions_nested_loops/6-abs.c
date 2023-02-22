02adfcac9e868e3d0798383a60f3b4fe7d825501
DanielleUju <danielleObilor1@gmail.com>
2023-02-22 09:03:20 -0800

    Absolute values

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,3 +3,17 @@
 /**
  * _abs - function that computes the absolute value of an integer
  *
+ * @a: parameter to be printed
+ *
+ * Return: a always
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
